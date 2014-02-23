76c63dc245baa05a06ef56a93d58cab0527c5660
JMaxwell <maxwell.joshua@gmail.com>
2014-02-23 15:40:42 -0500

    committing

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -7,11 +7,11 @@ var exampleBook = {
   bookName: 'Time for Bed',
   characters: {
     child: {
-      name: 'Bobby',
+      name: 'Character2',
       gender: 'male'
    },
     parent: {      
-      name: 'Mommy',
+      name: 'Character1',
       gender: 'female'
     }
   }      
@@ -19,61 +19,43 @@ var exampleBook = {
 
 
 var examplePages = [{
-  pageNumber: 'cover', //this could be 'cover'
-  imgUrl: '/images/add-a-photo-img.png',
-  verbalCue: 'child yawning',
-  dynamic: true,
-  renderCopyText: _.template( '' ),
-},{
-  pageNumber: 'b', //this could be 'cover'
-  imgUrl: '/images/add-a-photo-img.png',
-  verbalCue: 'child playing',
-  dynamic: true,
-  renderCopyText: _.template( '' ),
-},{
-  pageNumber: '1', //this could be 'cover'
-  imgUrl: '/images/add-a-photo-img.png',
-  verbalCue: 'child playing',
-  dynamic: true,
-  renderCopyText: _.template( 'It is late.<br>It is time for bed.<br>It is time to go to bed.' ),
-},{
-  pageNumber: '2', //this could be 'cover'
+  pageNumber: '1', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: 'child being silly in bathroom',
   dynamic: true,
   renderCopyText: _.template( '<span class="child"><%= child %></span> is funny.<br>Do not play, <span class="child"><%= child %></span>. <br>It is time to go to bed.' ),
 },{
-  pageNumber: '3', //this could be 'cover'
+  pageNumber: '3', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: 'getting ready for bathtime', 
   dynamic: true,
   renderCopyText: _.template( 'Come <span class="child"><%= child %></span>.<br>It is time.<br>It is time to wash.' ),
 },{
-  pageNumber: '4', //this could be 'cover'
+  pageNumber: '4', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: "child laying out next day's clothes", 
   dynamic: true,
   renderCopyText: _.template( 'Good, <span class="child"><%= child %></span>.<br>You can help.<br>Find the top.' ),
 },{
-  pageNumber: '5', //this could be 'cover'
+  pageNumber: '5', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: 'child crawling into bed',     
   dynamic: true,
   renderCopyText: _.template( 'Good <span class="child"><%= child %></span>.<br>Jump in bed.<br>You are good..' ),
 },{
-  pageNumber: '6', //this could be 'cover'
+  pageNumber: '6', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: 'reading in bed',      
   dynamic: true,
   renderCopyText: _.template( 'It is time.<br>It is time to read.<br>It is good to read.' ),
 },{
-  pageNumber: '7', //this could be 'cover'
+  pageNumber: '7', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: 'goodnight kiss',      
   dynamic: true,
   renderCopyText: _.template( 'It is time.<br>It is time to go to sleep.<br>You are good.' ),
 },{
-  pageNumber: '8', //this could be 'cover'
+  pageNumber: '8', 
   imgUrl: '/images/add-a-photo-img.png',
   verbalCue: 'turning off lights',              
   dynamic: true,
